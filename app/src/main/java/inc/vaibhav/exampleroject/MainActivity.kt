8865191723b773959dd72556d98826c50eaa6e8b
Vaibhav Jain <vaibhavjain30699@gmail.com>
2022-10-21 12:42:11 +0530

    Initial Commit

---
+++ app/src/main/java/inc/vaibhav/exampleroject/MainActivity.kt
@@ -0,0 +1,11 @@
+package inc.vaibhav.exampleroject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
